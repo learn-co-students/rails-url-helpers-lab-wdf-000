76dc6fd72e8ba9ca6fe99b41823af64e71b01bff
fidelscodes <fidelscodes@gmail.com>
2016-08-29 12:15:20 -0400

    Update with solution

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,25 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: :show
-  
+
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
+  end
+
+  def activate
+    @student = Student.find(params[:id])
+    if !@student.active
+      @student.active = true
+    else
+      @student.active = false
+    end
+    @student.save
+    
+    # Mind the syntax for redirect_to
+    redirect_to student_path(@student)
   end
 
   private
